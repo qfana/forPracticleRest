e0dd664c7e1532c00fdc5fb1246fbca36e6c6801
Nikita Borisov <qfana2123@gmail.com>
2024-09-13 22:14:08 +0300

    connect mongoose, add PostControl and router

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import router from './Router.js';
 
 const PORT = 7788;
+const DB_URL = 'mongodb+srv://qfana2123:TWiTwlbrrp8qgwZ3@cluster0.9eyu7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 const app = express();
 
 app.use(express.json()); // Парсинг передаваемой информации, преобразовывает в JSON формат
+app.use('/api', router);
 
-app.post('/', (req, res) => {
-  console.log(req.body); // Выводит GET запрос человека (postman) предварительно запрос проходит через парсер (7 строка)
-  res.status(200).json('Сервер работает'); // 200 = выполнено (для гет запроса, в данном случае не нужно, оставил что бы не забыть)
-});
 
-app.listen(PORT, () => { console.log(`Server started, port: ${PORT}`); }); // В случае если сервер запустился, без ошибки, выдает данный калбекк
+async function startApp() {
+  try {
+    await mongoose.connect(DB_URL);
+    app.listen(PORT, () => { console.log(`Server started, port: ${PORT}`); }); // В случае если сервер запустился, без ошибки, выдает данный калбекк
+  } catch (e) {
+    console.log(e);
+
+  }
+
+}
+
+startApp(); 
